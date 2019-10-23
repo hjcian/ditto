713a0024463982d112dc9ee9de0b49df9c9b9cb9
maxcian <max.cian@cellopoint.com>
2019-10-23 09:59:01 +0800

    feat(main): implement basic architecture

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import queue
 import random
 import threading
@@ -7,73 +8,74 @@ from concurrent.futures import ThreadPoolExecutor
 now = lambda : time.time() * 1000
 
 def mission(jobseq, jobid):
-    # consume = 0.5
-    # consume = random.random()
-    consume = random.randint(2,4)
-    # print(f"{jobseq} {jobid} ... consume {consume}")
-    time.sleep(consume)
-    # print(f"{jobseq} {jobid} done.")
-    return jobseq, jobid, consume
+    resp_time = random.randint(2,4)
+    time.sleep(resp_time)
+    return jobseq, jobid, resp_time
 
 def _concurrent_submit(jobseq, job_count):
-    print(f"[{jobseq}][{job_count}] entry _concurrent_submit")
     results = []
-    t0 = now()
     with ThreadPoolExecutor(max_workers=job_count) as executor:
-        print(f"[{jobseq}][{job_count}] entry ThreadPoolExecutor")
-        t1 = now()
         futures = [ executor.submit(mission, jobseq, jobid+1) for jobid in range(job_count) ]
-        print(f"[{jobseq}][{job_count}] before t2")
-        t2 = now()
         results = [ future.result() for future in futures ]
-        print(f"[{jobseq}][{job_count}] before t3")
-        t3 = now()
-        avg = (t2 - t1) / job_count
-        print(f"[{jobseq}][{job_count}] {t1-t0:.4f} {t2-t1:.4f} {job_count} {avg:.4f} ms =====")
-    return results
+    return {
+        "jobseq": jobseq,
+        "results": results
+    }
 
 class Ditto(object):
     def __init__(self):
-        self._max_workers = 10
-        self._executor = ThreadPoolExecutor(max_workers=self._max_workers)
-        self._queue = queue.SimpleQueue()
+        self._amplified_factor = 1.5
+        self._dynamic_workers = 10
+        self._executor = ThreadPoolExecutor(max_workers=self._dynamic_workers)
+        self._queue = queue.Queue()
+        self._isSubmitDone = False
+        self.results = []
         self._cunsumer = threading.Thread(target=self._consume)
         self._cunsumer.start()
-        self._hasJob = True
-        self.results = []
+
+    def _adjust_worker_count(self):
+        if len(self._executor._threads) == self._executor._max_workers:
+            self._dynamic_workers = int(self._dynamic_workers * self._amplified_factor)
+            self._executor._max_workers = self._dynamic_workers
 
     def _consume(self):
-        print("entry _consume")
         while True:
             try:
-                if self._queue.qsize():
-                    print(f"got {self._queue.qsize()}")
-                    result = self._queue.get()
-                    self.results.append(result.result())
-                if not self._hasJob:
+                if not self._queue.empty():
+                    result = self._queue.get().result()
+                    self.results.append(result)
+                    print(f"[_consume] jobseq {result['jobseq']} done. (qsize: {self._queue.qsize()}, # used workers: {len(self._executor._threads)}, max workers: {self._executor._max_workers} )")
+                elif self._isSubmitDone:
                     break
+                else:
+                    time.sleep(0.5) # prevent CPU up to 100%                    
             except Exception as e:
                 print(e)
                 break
+    
+    def _produce(self, jobseq, job_count):
+        result = self._executor.submit(_concurrent_submit, jobseq, job_count)
+        self._queue.put(result)
+        self._adjust_worker_count()
+        print(f"[_produce][{jobseq}] submit done. (qsize: {self._queue.qsize()}, # used workers: {len(self._executor._threads)}, max workers: {self._executor._max_workers} )")
 
     def run(self, jobs):
         for jobseq, job_count in enumerate(jobs):
-            result = self._executor.submit(_concurrent_submit, jobseq, job_count)
-            self._queue.put(result)
-            # result.result()
-            print(f"[{jobseq}] submit package done. (qsize: {self._queue.qsize()})")
+            self._produce(jobseq, job_count)
             time.sleep(1)
 
-        self._hasJob = False
+        self._isSubmitDone = True
         self._cunsumer.join()
         print("-=-=-=-=")
 
 if __name__ == "__main__":
-    jobs = [ random.randint(10, 50) for _ in range(5)]
-    ditto = Ditto()
-    time.sleep(1)
-    ditto.run(jobs)
-    print("done job")
-    
-    print(ditto.results)
-    print(len(ditto.results))
+    try:
+        total = 20
+        jobs = [ random.randint(10, 50) for _ in range(total)]
+        ditto = Ditto()
+        time.sleep(1)
+        ditto.run(jobs)
+        print("done job")
+        print(len(ditto.results))
+    except KeyboardInterrupt:
+        sys.exit(1)
